2f95cf63db36e5f1ac7fa6863d0c5d55b04c4e51
Diego Fernandes <diegofernandes62@hotmail.com>
2020-11-13 22:23:50 -0300

    Sort algoritmos

---
+++ Algoritimos de Ordenacao/Selection_Sort.py
@@ -0,0 +1,62 @@
+# Selection Sort pega o menor elemento de uma lista e o coloca em primeiro e assim sucessivamente
+
+'''
+Como eu determino o menor elemento de uma lista?
+
+'''
+a = [7,5,1,8,3]
+
+
+def selection_sort(a):
+    minimo = [0]
+    for i in range(len(a)):
+        if a[i] < minimo:
+            minimo = a[i]
+    j = 0
+    if a[j] > minimo:
+        
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define TAM 10000
+def performance
+    x = 0
+    y = 0
+    aux = 0
+    inicio = 0
+    final = 0
+    tempo_decorrido = 0.0
+    vetor = [len]
+    print("Gerando um vetor com %i inteiros aleatorios:", vetor)
+
+for(x = 0; x < vetor; x++):
+    vetor[x] = (int(randint(0,100)) % 100) 
+    print(vetor[x])
+    
+    input()
+    inicio = time.clock;
+
+    long int before = GetTickCount();
+    long int after = GetTickCount();
+ for( x = 0; x < TAM; x++ )
+ {
+ for( y = x + 1; y < TAM; y++ )
+ {
+ if ( vetor[x] > vetor[y] )
+ {
+ aux = vetor[x];
+ vetor[x] = vetor[y];
+ vetor[y] = aux;
+ }
+ }
+ }
+ final = GetTickCount();
+ tempo_decorrido = final - inicio;
+ printf("\nTempo de Ordenacao = : %d milisegundos ",tempo_decorrido);
+ getchar();
+ printf("\nVetor Ordenado\n ");
+ for( x = 0; x < TAM; x++ )
+ printf("%d\t",vetor[x]);
+syst
+
---
+++ Algoritimos de Ordenacao/test.py
@@ -0,0 +1,17 @@
+
+import time
+
+t0 = (time.clock())
+print(t0)
+#
+
+
+
+# def numaleatório():
+#     import random
+#     global p
+    
+#     global chave
+#     p = random.randrange(0,1000000000000)
+
+
